5e325a52920aba9d51ec0f79e7c3d230d855fc30
Cong Liu <stormliucong@gmail.com>
2024-07-11 20:12:58 -0400

    make same callback page if success hide register show use button

--- patient-portal/src/App.js
+++ patient-portal/src/App.js
@@ -1,6 +1,5 @@
 // Main App Component with Routing
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RegistrationCallback from './components/RegistrationCallback';
 import NavigateToServiceApp from './components/NavigateToServiceApp';
 import Home from './components/Home';
 
@@ -10,7 +9,6 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/navigate-to-service" element={<NavigateToServiceApp />} />
-                <Route path="/patient-callback" element={<RegistrationCallback />} />
                 {/* Other routes */}
             </Routes>
         </Router>
--- patient-portal/src/components/NavigateToServiceApp.js
+++ patient-portal/src/components/NavigateToServiceApp.js
@@ -1,23 +1,45 @@
 // React Component to Navigate to Service App
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Home from './Home';
 
 const NavigateToServiceApp = () => {
-    const navigate = useNavigate();
+    const [isRegistered, setIsRegistered] = useState(false);
+    const location = useLocation();
+    useEffect(() => {
+        // Check if the registration status is in the URL query params
+        const query = new URLSearchParams(location.search);
+        const status = query.get('status');
+        if (status === 'success') {
+            setIsRegistered(true);
+        }
+    }, [location]);
 
     const handleNavigate = () => {
-        const callbackUrl = `${window.location.origin}/patient-callback`;
+        const callbackUrl = `${window.location.origin}/navigate-to-service`;
         const serviceAppUrl = `http://localhost:3000`;
         console.log('callbackUrl:', callbackUrl);
         const serviceAppRegisterUrl = `${serviceAppUrl}/register?callbackUrl=${encodeURIComponent(callbackUrl)}`;
         window.open(serviceAppRegisterUrl, '_self');  // Opens in the same tab or specify '_blank' for a new tab
     };
 
+    const handleUseService = () => {
+        console.log('Using the service...');
+        // Implement the action to use the service
+    };
+
     return (
         <React.Fragment>
             <Home />
-            <button onClick={handleNavigate}>Register on Service App</button>
+            <div>
+            {isRegistered ? (
+                <div>
+                    <button onClick={handleUseService}>Use Service</button>
+                </div>
+            ) : (
+                <button onClick={handleNavigate}>Register on Service App</button>
+            )}
+        </div>
         </React.Fragment>
     );
 }
